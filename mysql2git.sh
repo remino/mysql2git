06c1499dc347d0cb77d2ae9529172ce8818875b7
Remi Plourde <remi@remino.net>
2012-01-30 14:23:37 +0900

    Move code in functions

--- mysql2git.sh
+++ mysql2git.sh
@@ -15,7 +15,66 @@
 # http://www.linuxjournal.com/node/1001956
 #
 
-if [ $# -lt 1 ]; then
+mysql2git_main() {
+  [ $# -lt 1 ] && mysql2git_usage
+
+  GITCIMSG=""
+  MYSQLARGS=""
+
+  while getopts 'u:p:h:m:' opt; do
+    case $opt in
+      u) MYSQLARGS="$MYSQLARGS -u $OPTARG";;
+      p) MYSQLARGS="$MYSQLARGS -p$OPTARG";;
+      h) MYSQLARGS="$MYSQLARGS -h $OPTARG";;
+      m) GITCIMSG="$OPTARG";;
+    esac
+  done
+  shift $(( OPTIND-1 ))
+
+  DUMPDIR=$1
+  [ ! -d "$DUMPDIR" ] && mkdir "$DUMPDIR"
+  cd "$DUMPDIR"
+
+  [ ! -e ".git" ] && git init .
+
+  shift
+  if [ $# -lt 1 ]; then
+    DBLIST=`echo show databases | mysql --vertical=false | tail -n +2`
+  else
+    DBLIST="$@"
+  fi
+
+  set +o noclobber
+  for db in $DBLIST; do
+    DBDIR="$db"
+    TABLELIST=`echo show tables | mysql -D $db --vertical=false | tail -n +2`
+    [ -d $DBDIR ] && rm -fr $DBDIR
+    mkdir -p $DBDIR
+    for table in $TABLELIST; do
+      tabledir="$DBDIR/$table"
+      mkdir -p "$tabledir"
+      mysqldump --skip-extended-insert --skip-dump-date --no-data \
+        $db $table > "$tabledir/schema.sql"
+      mysqldump --skip-extended-insert --skip-dump-date --no-create-info \
+        $db $table > "$tabledir/data.sql"
+    done
+  done
+
+  git add .
+
+  GITRMFILES="$( git ls-files --deleted )"
+  if [ "$GITRMFILES" != "" ]; then
+    git rm $GITRMFILES
+  fi
+
+  if [ "$GITCIMSG" == "" ]; then
+    git commit
+  else
+    git commit -m "$GITCIMSG"
+  fi
+}
+
+mysql2git_usage() {
   echo "Usage: `basename $0` [options] dump-dir [db1 db2...]"
   echo ""
   echo "  options:"
@@ -24,59 +83,6 @@ if [ $# -lt 1 ]; then
   echo "    -m message"
   echo "      Git commit message."
   exit 1
-fi
+}
 
-GITCIMSG=""
-MYSQLARGS=""
-
-while getopts 'u:p:h:m:' opt; do
-  case $opt in
-    u) MYSQLARGS="$MYSQLARGS -u $OPTARG";;
-    p) MYSQLARGS="$MYSQLARGS -p$OPTARG";;
-    h) MYSQLARGS="$MYSQLARGS -h $OPTARG";;
-    m) GITCIMSG="$OPTARG";;
-  esac
-done
-shift $(( OPTIND-1 ))
-
-DUMPDIR=$1
-[ ! -d "$DUMPDIR" ] && mkdir "$DUMPDIR"
-cd "$DUMPDIR"
-
-[ ! -e ".git" ] && git init .
-
-shift
-if [ $# -lt 1 ]; then
-  DBLIST=`echo show databases | mysql --vertical=false | tail -n +2`
-else
-  DBLIST="$@"
-fi
-
-set +o noclobber
-for db in $DBLIST; do
-  DBDIR="$db"
-  TABLELIST=`echo show tables | mysql -D $db --vertical=false | tail -n +2`
-  [ -d $DBDIR ] && rm -fr $DBDIR
-  mkdir -p $DBDIR
-  for table in $TABLELIST; do
-    tabledir="$DBDIR/$table"
-    mkdir -p "$tabledir"
-    mysqldump --skip-extended-insert --skip-dump-date --no-data \
-      $db $table > "$tabledir/schema.sql"
-    mysqldump --skip-extended-insert --skip-dump-date --no-create-info \
-      $db $table > "$tabledir/data.sql"
-  done
-done
-
-git add .
-
-GITRMFILES="$( git ls-files --deleted )"
-if [ "$GITRMFILES" != "" ]; then
-  git rm $GITRMFILES
-fi
-
-if [ "$GITCIMSG" == "" ]; then
-  git commit
-else
-  git commit -m "$GITCIMSG"
-fi
+mysql2git_main $@
