6f784fcf350af7b2588cc831d14ca49647af6975
Remi Plourde <remi@remino.net>
2012-01-30 14:12:15 +0900

    Take care of git commits

--- mysql2git.sh
+++ mysql2git.sh
@@ -16,11 +16,34 @@
 #
 
 if [ $# -lt 1 ]; then
-  echo "Usage: `basename $0` dump-dir [db1 db2...]"
+  echo "Usage: `basename $0` [options] dump-dir [db1 db2...]"
+  echo ""
+  echo "  options:"
+  echo "    -u username -p password -h host"
+  echo "      MySQL username, password, and host."
+  echo "    -m message"
+  echo "      Git commit message."
   exit 1
 fi
 
+GITCIMSG=""
+MYSQLARGS=""
+
+while getopts 'u:p:h:m:' opt; do
+  case $opt in
+    u) MYSQLARGS="$MYSQLARGS -u $OPTARG";;
+    p) MYSQLARGS="$MYSQLARGS -p$OPTARG";;
+    h) MYSQLARGS="$MYSQLARGS -h $OPTARG";;
+    m) GITCIMSG="$OPTARG";;
+  esac
+done
+shift $(( OPTIND-1 ))
+
 DUMPDIR=$1
+[ ! -d "$DUMPDIR" ] && mkdir "$DUMPDIR"
+cd "$DUMPDIR"
+
+[ ! -e ".git" ] && git init .
 
 shift
 if [ $# -lt 1 ]; then
@@ -31,13 +54,10 @@ fi
 
 set +o noclobber
 for db in $DBLIST; do
-  DBDIR=$DUMPDIR/$db
-  DATADIR=$DBDIR/data
-  SCHEMADIR=$DBDIR/schema
+  DBDIR="$db"
   TABLELIST=`echo show tables | mysql -D $db --vertical=false | tail -n +2`
-  [ -d $DBDIR ] || mkdir $DBDIR
-  [ -d $DATADIR ] || mkdir $DATADIR
-  [ -d $SCHEMADIR ] || mkdir $SCHEMADIR
+  [ -d $DBDIR ] && rm -fr $DBDIR
+  mkdir -p $DBDIR
   for table in $TABLELIST; do
     tabledir="$DBDIR/$table"
     mkdir -p "$tabledir"
@@ -47,3 +67,16 @@ for db in $DBLIST; do
       $db $table > "$tabledir/data.sql"
   done
 done
+
+git add .
+
+GITRMFILES="$( git ls-files --deleted )"
+if [ "$GITRMFILES" != "" ]; then
+  git rm $GITRMFILES
+fi
+
+if [ "$GITCIMSG" == "" ]; then
+  git commit
+else
+  git commit -m "$GITCIMSG"
+fi
